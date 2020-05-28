1bd29e51f992db9bb57b600306d498bbe51a013f
lamayine <58402836+lamayine@users.noreply.github.com>
2020-05-28 14:37:42 +0200

    Update Index.php

--- Index.php
+++ Index.php
@@ -4,3 +4,5 @@ require_once __DIR__ . '/vendor/autoload.php';
 use wishlist\conf\ConnectionFactory as CF;
 use wishlist\divers\Outils;
 use wishlist\divers\Formulaire;
+
+// fais moi signe mahmoud si tu lis sa
