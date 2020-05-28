7d50bded251f083cbb9561768135338391a42b90
lamayine <58402836+lamayine@users.noreply.github.com>
2020-05-28 02:20:45 +0200

    création de la class Message.php

---
+++ src/modeles/Message.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace wishlist\modele;
+
+
+class Message extends \Illuminate\Database\Eloquent\Model
+{
+	
+    protected $table = 'message';
+    protected $primaryKey = 'id';
+    public $timestamps = false;
+
+    public function liste() {
+        return $this->belongsTo('wishlist\modele\Liste' , 'id_message');
+    }
+
+}
