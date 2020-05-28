cb2c6fc4b6956811ee61c793ab61e26089483b96
lamayine <58402836+lamayine@users.noreply.github.com>
2020-05-28 02:40:30 +0200

    Create ConnectionFactory.php

---
+++ src/config/ConnectionFactory.php
@@ -0,0 +1,13 @@
+?php
+  
+namespace wishlist\conf;
+
+use Illuminate\Database\Capsule\Manager as DB;
+
+
+class ConnectionFactory {
+
+    private static $config = null;
+    private static $db = null;
+
+// à suivre
