064bee68d29d3e70d16c196175a7d8eeaf3e7b6f
lamayine <58402836+lamayine@users.noreply.github.com>
2020-05-28 16:34:54 +0200

    Update Index.php

--- Index.php
+++ Index.php
@@ -5,4 +5,11 @@ use wishlist\conf\ConnectionFactory as CF;
 use wishlist\divers\Outils;
 use wishlist\divers\Formulaire;
 
+use wishlist\fonction\Authentification as AUTH;
+use wishlist\fonction\Alerte;
+use wishlist\fonction\FctnListe as FL;
+use wishlist\fonction\FctnCagnotte as CG;
+use wishlist\fonction\CreateurItem as CI;
+use wishlist\fonction\GestionImage as GI;
+
 // fais moi signe mahmoud si tu lis sa
